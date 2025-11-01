284be007c1c9ceee43094e1855ace01707880233
VenkatJayaPrakash14 <venkatjayaprakash14@gmail.com>
2025-11-01 12:44:28 +0530

    changes

--- Pattern-1.py
+++ Pattern-1.py
@@ -4,3 +4,16 @@ for i in range(r):
     for j in range(c):
             print('*',end='')
     print()  
+
+
+
+
+r=5
+c=10
+for i in range(r):
+    for j in range(c):
+            print('*',end='')
+            if j!=c-1:
+                  print('_', end='')
+            
+    print()  
