1684b881e3f82e72c7f7b1f362c41a630f518624
VenkatJayaPrakash14 <venkatjayaprakash14@gmail.com>
2025-11-16 19:32:17 +0530

    time complexcity

--- P121-Best Time to Buy and Sell Stock.py
+++ P121-Best Time to Buy and Sell Stock.py
@@ -1,8 +1,8 @@
 prices=[7,1,2,4,6,5]
 n=len(prices)
+minv=prices[0]
 ans=0
-for i in range(n):
-    for j in range(i+1,n):
-        temp=prices[j]-prices[i]
-        ans=max(ans,temp)
+for i in range(1,len(prices)):
+    ans=max(ans,(prices[i]-minv))
+    minv=min(minv,prices[i])
 print(ans)
