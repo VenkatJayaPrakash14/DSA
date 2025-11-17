416fb55cdf9741bdeb3a9a90fe0f5e62c85ea50f
VenkatJayaPrakash14 <venkatjayaprakash14@gmail.com>
2025-11-17 15:50:00 +0530

    1stTime

---
+++ P2016-Maximum Difference Between Increasing Elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        minV=nums[0]
+        ans=0
+        for i in range(1,len(nums)):
+            ans=max(ans,(nums[i]-minV))
+            minV=min(minV,nums[i])
+        if ans==0:
+            ans=-1 
+       
+        return ans
--- rough.py
+++ rough.py
@@ -1,20 +1,7 @@
-# r=10
-# c=5
-# for i in range(r):
-#     for j in range(c):
-#         if i==0 or i==r-1 or j==0 or j==c-1:
-#             print('*',end='')
-#         else:
-#             print(' ',end='')
-#     print()
-
-
-r=5
-c1=r-1
-c2=4
-for i in range(r):
-    for j in range(i):
-        print(' ',end='')
-    for k in range(c2-i):
-        print('* ',end='')
-    print()
+a=[7,1,5,4]
+minV=a[0]
+ans=0
+for i in range(1,len(a)):
+    ans=max(ans,(a[i]-minV))
+    minV=min(minV,a[i])
+print(ans)
